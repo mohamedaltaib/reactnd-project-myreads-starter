23d47eef162b2171acb27fc339b2b2dbfb79bc3e
Mohamed Altaib <m_altyeib@yahoo.com>
2020-04-07 20:46:29 +0200

    convert SearchBox to function component

--- src/components/searchScreen/SearchBox.js
+++ src/components/searchScreen/SearchBox.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import BackButton from "./BackButton";
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <BackButton syncSearch={this.props.syncSearch} />
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              onChange={e => this.props.searchBooks(e.target.value)}
-            />
-          </div>
+const SearchBox = props => {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <BackButton syncSearch={props.syncSearch} />
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            onChange={e => props.searchBooks(e.target.value)}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SearchBox;
